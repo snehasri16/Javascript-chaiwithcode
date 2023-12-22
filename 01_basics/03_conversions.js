c520e76aeaf9739f208ecab7e304f529ad6e01a2
SNEHA SRI ADUPA <117506229+snehasri16@users.noreply.github.com>
2023-12-22 11:11:00 +0000

    conversions done

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,48 @@
+
+let score = "sneha"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "sneha"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "sneha" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " sneha"
+
+let str3 = str1 + str2
+console.log(str3);
